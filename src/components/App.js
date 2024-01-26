cc4265b22c59a8d1775aa63a0b02bb7ebc9229f3
Franck kapuya <franckkapuya13@gmail.com>
2024-01-26 23:46:14 +0100

    utilisation de boucle et des conditions

--- src/components/App.js
+++ src/components/App.js
@@ -2,11 +2,13 @@ import logo from '../logo.svg';
 import '../App.css';
 import Banner from './Banner';
 import Cart from './Cart';
+import ShoppingList from './ShoppingList';
 
 function App() {
   return (
       <div>
         <Banner />
+        <ShoppingList />
         <Cart />
       </div> 
   ) 
---
+++ src/components/ShoppingList.js
@@ -0,0 +1,19 @@
+import { plantList } from "../datas/plantList"
+
+function ShoppingList() {
+    const renderedItems = [];
+  
+    plantList.forEach((plant) => {
+        //ici on fait une boucle sur nos plante et on utilise l'id comme key 
+        renderedItems.push(<li key={plant.id}>{plant.name} {plant.isBestSale ? <span>🔥</span> : null}</li> );
+      });
+    return (
+        <ul>
+
+        {renderedItems}
+
+        </ul>
+    )
+}
+
+export default ShoppingList
